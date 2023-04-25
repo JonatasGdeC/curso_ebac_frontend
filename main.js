bc3cd258357b07c4edeb9ccd0fbc8fb3648ac866
Jônatas <jonatas.07022003@gmail.com>
2023-04-24 23:23:33 -0300

    Update main.js
    Correção de erro "Tarefa Riscada"

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
-    const novoItem = $('<li style="display:block"></li>');
-    
+
     $('form').on('submit', function(e){
         e.preventDefault();
         const novaTarefa = $('#input-tarefa').val();
 
-        $(`<p>${novaTarefa}</p>`).appendTo(novoItem);
-        $(novoItem).appendTo('ul');
-        $('#input-tarefa').val('')
-    })
-    $(novoItem).click(function(){
-        $('li').css({textDecoration: "line-through"});
+        const novoItem = $(`<li>${novaTarefa}</li>`).appendTo('ul');
+        $('#input-tarefa').val('');
+
+        $(novoItem).click(function(){
+            $(this).css({textDecoration: "line-through"});
+        })
     })
 })
